e9c50b576ed8964307fd0d5b1349a42a6b3d8606
Vishalghyv <vishal38785@gmail.com>
2020-11-04 22:10:38 +0530

    Changing basic structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/Counter';
+import './App.css';
+import SideBar from './features/SideBar';
+
+function App() {
+  return (
+    <div className="App">
+        // SIDEBAR
+        <SideBar />
+        // CHAT COMMENT
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./SideBar.css"
+function SideBar() {
+    return (
+        <div className="sidebar">
+            <h2>Hellsdso</h2>
+            <div className="sidebar__top">
+                <h2>Hellsdso</h2>
+            </div>
+        </div>
+
+    )
+}
+
+export default SideBar;
